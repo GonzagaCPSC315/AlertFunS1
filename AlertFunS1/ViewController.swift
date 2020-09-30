77f574884613754b398b5200777c9e78a0fa1d7e
Gina Sprint <sprint@gonzaga.edu>
2020-09-30 10:45:58 -0700

    end of AlertFun on Wednesday

--- AlertFunS1/ViewController.swift
+++ AlertFunS1/ViewController.swift
@@ -15,6 +15,21 @@ class ViewController: UIViewController {
     @IBAction func echoBackPressed(_ sender: UIButton) {
         if let text = textField.text {
             print(text)
+            // MARK: - Alerts
+            // there are two types of alerts
+            // actionSheet: show to the user when they expect some user input (e.g. delete some values and you want to confirm the delete)
+            // alert: show to the user when something goes wrong and they don't expect user input/messsage
+            // use a UIAlertController to set up our alert
+            let alertController = UIAlertController(title: "Echo Back", message: text, preferredStyle: .alert)
+            // lets add a UIAlertAction to our alert dialog
+            alertController.addAction(UIAlertAction(title: "Okay", style: .default, handler: { (action) -> Void in
+                print("User pressed okay")
+            }))
+            // show the alert with present()
+            present(alertController, animated: true, completion: { () -> Void in
+                print("Just presented the alert")
+            })
+            
         }
     }
     // task: in IB, add a UITextField that is as wide as the iPhone SE screen
